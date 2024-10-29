e2c328c6c28696496d07c1048b7e3ef87ac3f655
Wassim <wassimwassim.ka1@gmail.com>
2024-10-29 12:05:18 +0100

    upload new files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html","./pages/**/*.html","./script/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        textSpecial: "#F8A73E",
+      },
+    },
+  },
+  plugins: [],
+}
+
